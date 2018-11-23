678f9e8c5e5eca2cdb1c453217c7926d763a940f
Mukesh Sharma <msharma@calance.com>
2018-11-23 19:32:36 -0400

    Updated files

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ cd azure/vm_setup
 chmod +x ../../binaries/terraform_linux
 ../../binaries/terraform_linux init 
 ../../binaries/terraform_linux validate
-../../binaries/terraform_linux destroy -auto-approve 
+../../binaries/terraform_linux apply -auto-approve 
